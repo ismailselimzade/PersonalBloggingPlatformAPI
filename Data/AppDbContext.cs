daf8ec1bc1c2823cb745ccbdd7db2dc17e249033
Ismayil Selimzade <ismailselimzade@mail.ru>
2026-07-14 02:46:56 +0400

    feat: add AppDbContext

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PersonalBloggingPlatformAPI.Models;
+
+namespace PersonalBloggingPlatformAPI.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Article> Articles { get; set; }
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+    }
+}
