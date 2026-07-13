f43cd46b7cb248a563d91adc6aca8bea03f2f03e
Ismayil Selimzade <ismailselimzade@mail.ru>
2026-07-14 03:24:51 +0400

    feat: setup database and migrations

---
+++ Data/AddDbContextFactory.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace PersonalBloggingPlatformAPI.Data
+{
+    public class AppDbContextFactory : IDesignTimeDbContextFactory<AppDbContext>
+    {
+        public AppDbContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<AppDbContext>();
+         
+            optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=PersonalBloggingDb;Trusted_Connection=True;");
+
+            return new AppDbContext(optionsBuilder.Options);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,5 +1,10 @@
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
+
+builder.Services.AddDbContext<PersonalBloggingPlatformAPI.Data.AppDbContext>(options =>
+    options.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=PersonalBloggingDb;Trusted_Connection=True;"));
 
 app.MapGet("/", () => "Hello World!");
 
