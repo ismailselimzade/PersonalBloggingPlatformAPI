6b3d280e098d92d7cd7674d55233b00d6b6a8b41
Ismayil Selimzade <ismailselimzade@mail.ru>
2026-07-15 01:45:24 +0400

    feat: add Author property to Article model and update database schema

---
+++ Migrations/20260714214456_AddAuthorToArticle.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PersonalBloggingPlatformAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddAuthorToArticle : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Author",
+                table: "Articles",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Author",
+                table: "Articles");
+        }
+    }
+}
--- Models/Article.cs
+++ Models/Article.cs
@@ -7,6 +7,7 @@ namespace PersonalBloggingPlatformAPI.Models
         public int Id { get; set; }
         public string Title { get; set; }
         public string Content { get; set; }
+        public string Author { get; set; }
         public DateTime CreatedAt { get; set; }
     }
 }
