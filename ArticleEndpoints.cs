456e922ed37eb79f83f6c087f31306c7450cabdc
Ismayil Selimzade <ismailselimzade@mail.ru>
2026-07-15 00:10:02 +0400

    refactor: move article endpoints to separate class for better modularity

---
+++ ArticleEndpoints.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Routing.Patterns;
+using PersonalBloggingPlatformAPI.Data;
+using PersonalBloggingPlatformAPI.Models;
+
+namespace PersonalBloggingPlatformAPI
+{
+    public class ArticleEndpoints
+    {
+        public static RouteGroupBuilder routeGroupBuilder(WebApplication app)
+        {
+            var group = app.MapGroup("/articles");
+
+            group.MapPost("", async ([FromBody] Article article, AppDbContext db) =>
+            {
+                db.Articles.Add(article);
+                await db.SaveChangesAsync();
+                return Results.Ok(article);
+            });
+
+            group.MapGet("", async (AppDbContext db) =>
+            {
+                var articles = await db.Articles.ToListAsync();
+                return Results.Ok(articles);
+            });
+
+            group.MapGet("/{id}", async (AppDbContext db, int id) =>
+            {
+                var article = await db.Articles.FindAsync(id);
+                return article == null ? Results.NotFound() : Results.Ok(article);
+            });
+
+            group.MapPut("/{id}", async ([FromBody] Article updateArticle, AppDbContext db, int id) =>
+            {
+                var articles = await db.Articles.FindAsync(id);
+
+                if (articles != null)
+                {
+                    articles.Title = updateArticle.Title;
+                    articles.Content = updateArticle.Content;
+                    await db.SaveChangesAsync();
+
+                    return Results.Ok(articles);
+                }
+                return Results.NotFound();
+            });
+
+            group.MapDelete("/{id}", async (AppDbContext db, int id) =>
+            {
+                var articles = await db.Articles.FindAsync(id);
+
+                if (articles != null)
+                {
+                    db.Remove(articles);
+                    await db.SaveChangesAsync();
+                    return Results.NoContent();
+                }
+
+                return Results.NotFound();
+            });
+
+            return group;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using PersonalBloggingPlatformAPI;
 using PersonalBloggingPlatformAPI.Data;
 using PersonalBloggingPlatformAPI.Models;
 
@@ -19,54 +20,6 @@ if (builder.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-app.MapGet("/", () => "Hello World!");
-
-app.MapPost("/articles", async ([FromBody] Article article, AppDbContext db) =>
-{
-    db.Articles.Add(article);
-    await db.SaveChangesAsync();
-    return Results.Ok(article);
-});
-
-app.MapGet("/articles", async (AppDbContext db) =>
-{
-    var articles = await db.Articles.ToListAsync();
-    return Results.Ok(articles);
-});
-
-app.MapGet("/articles/{id}", async (AppDbContext db, int id) =>
-{
-    var article = await db.Articles.FindAsync(id);
-    return article == null ? Results.NotFound() : Results.Ok(article);
-});
-
-app.MapPut("articles/{id}", async ([FromBody] Article updateArticle, AppDbContext db, int id) =>
-{
-    var articles = await db.Articles.FindAsync(id);
-
-    if (articles != null)
-    {
-        articles.Title = updateArticle.Title;
-        articles.Content = updateArticle.Content;
-        await db.SaveChangesAsync();
-
-        return Results.Ok(articles);
-    }
-    return Results.NotFound();
-});
-
-app.MapDelete("/articles/{id}", async (AppDbContext db, int id) =>
-{
-    var articles = await db.Articles.FindAsync(id);
-
-    if (articles != null)
-    {
-        db.Remove(articles);
-        await db.SaveChangesAsync();
-        return Results.NoContent();
-    }
-
-    return Results.NotFound();
-});
+ArticleEndpoints.routeGroupBuilder(app);
 
 app.Run();
